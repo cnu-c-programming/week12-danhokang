d8d7165efca5ace8dedd22c8c4ca5bddf83b5c8a
danhokang <danho2898@naver.com>
2026-05-22 09:47:08 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
-int main(int argc, const char* argv[]) {
-    if (argc < 2)
+int main(int argc, const char *argv[]) {
+    if (argc < 2) {
         return 0;
+    }
 
-    FILE* fp = fopen(argv[1], "r");
+    FILE *fp = fopen(argv[1], "r");
 
+    if (fp == NULL) {
+        return 0;
+    }
 
+    int count[26] = {0};
+    int c;
+
+    while ((c = fgetc(fp)) != EOF) {
+        if (c >= 'a' && c <= 'z') {
+            count[c - 'a']++;
+        }
+    }
 
     fclose(fp);
-}
 
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, count[i]);
+    }
+
+    return 0;
+}
