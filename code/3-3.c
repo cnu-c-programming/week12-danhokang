31529ded374ed6b33aecdec4b8852e9d2631c57b
danhokang <danho2898@naver.com>
2026-05-22 09:51:05 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 
-int main(int argc, const char* argv[]) {
-    const char* filename = "myfile.txt";
+int main(int argc, const char *argv[]) {
+    const char *filename = "myfile.txt";
 
+    FILE *fp1 = fopen(filename, "r");
+    FILE *fp2 = fopen(filename, "w");
 
-    "fp1 %s %p %d %d\n"
-    "fp2 %s %p %d %d\n"
-    
+    if (fp1 == NULL || fp2 == NULL) {
+        return 0;
+    }
+
+    char tmp[256];
+
+    while (fgets(tmp, sizeof(tmp), fp1) != NULL) {
+    }
+
+    char buf1[256] = {0};
+    char buf2[256] = {0};
+
+    char *res_fp1 = fgets(buf1, sizeof(buf1), fp1);
+    char *res_fp2 = fgets(buf2, sizeof(buf2), fp2);
+
+    printf("fp1 %s %p %d %d\n", buf1, res_fp1, ferror(fp1), feof(fp1));
+    printf("fp2 %s %p %d %d\n", buf2, res_fp2, ferror(fp2), feof(fp2));
+
+    fclose(fp1);
+    fclose(fp2);
 
     return 0;
 }
