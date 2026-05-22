9555c2ba88268ccf08a080e4c2d28ffd38f433ba
danhokang <danho2898@naver.com>
2026-05-22 09:47:46 +0900

    Update 2-6.c

--- code/2-6.c
+++ code/2-6.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, const char* argv[]) {
-    if (argc < 3)
+int main(int argc, const char *argv[]) {
+    if (argc < 3) {
         return 0;
+    }
 
-    FILE* fp = fopen(argv[1], "r");
-    const char* target_str = argv[2];
+    FILE *fp = fopen(argv[1], "r");
 
+    if (fp == NULL) {
+        return 0;
+    }
 
-    
+    const char *target_str = argv[2];
+    char buffer[256];
+
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        if (strstr(buffer, target_str) != NULL) {
+            printf("%s", buffer);
+        }
+    }
+
     fclose(fp);
-}
 
+    return 0;
+}
